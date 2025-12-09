cbb1866c86f3a3d92450d5a21fd137d169dc8179
Rutahiwa <derickfaustine7@gmail.com>
2025-12-09 23:54:27 +0300

    added models.py for accounts and newsletter

--- newsletters/models.py
+++ newsletters/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Newsletter(models.Model):
+    STATUS_CHOICES = [
+        ("PENDING", "Pending"),
+        ("SENT", "Sent"),
+    ]
+
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="PENDING")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
